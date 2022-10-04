4fa674a47d9d0cfdf02f3f8150b1472e98acc1b4
RobsonTrasel <robson.trasel@gmail.com>
2022-10-03 21:25:35 -0300

    chore: initial add

---
+++ app/Http/Controllers/MarcaControllerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MarcaController;
+use Illuminate\Http\Request;
+
+class MarcaControllerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MarcaController  $marcaController
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MarcaController $marcaController)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MarcaController  $marcaController
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MarcaController $marcaController)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MarcaController  $marcaController
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MarcaController $marcaController)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MarcaController  $marcaController
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MarcaController $marcaController)
+    {
+        //
+    }
+}
